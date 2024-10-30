e2f58954ee6559f2250db820134c410791543d54
24HP9588 <24hp9588@G305-CC0060.local>
2024-10-30 17:46:30 +0800

    lint change

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,13 @@
-
 import { Blogs } from "@/components/blogs";
 import { Carousels } from "@/components/carousels";
 import { TrendCard } from "@/components/TrendCard";
-
 
 export default function Home() {
   return (
     <main>
       <Carousels />
-      <TrendCard />
-      <Blogs />
+      {/* <TrendCard /> */}
+      {/* <Blogs /> */}
     </main>
   );
 }
